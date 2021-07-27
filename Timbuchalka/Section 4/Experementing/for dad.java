ad990ffeb4d0d67083ed89cd36805356a8efba4c
Advik-B <advik.b@gmail.com>
2021-07-27 09:50:21 +0530

    Updated the files

--- Timbuchalka/Section 4/Experementing/for dad.java
+++
@@ -1,7 +0,0 @@
-class A {
-    public static void main(String args[]){
-        System.out.println("Hello World");
-        System.out.println("Hi nana");
-        System.out.println("gg");
-    }
-   }
