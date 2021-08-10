f69f439cc2b909a5b0fe3c2e61cbf230bf1871af
Advik-B <advik.b@gmail.com>
2021-08-10 15:57:53 +0530

    Update grid_manager.py

---
+++ TKINTER/Udemy/grid_manager.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+entry = ttk.Entry(root , width=30)
+entry2 = ttk.Entry(root , width=30)
+entry.insert(0 , 'Please enter your name')
+entry2.insert(0 , 'Please enter your password')
+
+button = ttk.Button(root , text='Enter')
+lbltitle = ttk.Label(root , text='Our title here' , font='Consolas 22')
+lblname = ttk.Label(root , text='Your name :')
+lblpassword = ttk.Label(root , text='Your password :')
+
+lbltitle.grid( row = 0 , column = 0 , columnspan=2)
+lblname.grid(row = 1 , column = 0 , sticky=W)
+lblpassword.grid( row = 2  ,column = 0)
+entry.grid(row = 1 , column = 1)
+entry2.grid( row = 2 , column = 1)
+button.grid(row = 3 , column = 1, sticky=E+W , pady=5)
+
+root.geometry('500x400')
+root.mainloop()
