316d39eb287789eb82fdb42a6b4cbf725c8e27c2
Advik-B <advik.b@gmail.com>
2021-09-21 14:42:16 +0530

    Update virus.py

--- Timbuchalka/Section-5/experementing/virus.py
+++ Timbuchalka/Section-5/experementing/virus.py
@@ -1,17 +1,19 @@
-import os
-import time 
+# import os
+# import time 
 
-f = open ("TROOL.txt",'w+')
+# f = open ("TROOL.txt",'w+')
 
-a = open ("VIRUS.txt",'w+')
+# a = open ("VIRUS.txt",'w+')
 
-for i in range (0,999):
-    os.mkdir(f"{i}")
-    f.write(f"I printed this {i-1} times\n")
-    for q in range (0,i-q):
-        a.write("GET TROOOLED\n")
+# for i in range (0,999):
+#     os.mkdir(f"{i}")
+#     f.write(f"I printed this {i-1} times\n")
+#     for q in range (0,i-q):
+#         a.write("GET TROOOLED\n")
 
-f.close
-a.close
-print ("Wow u made it through all this shit!!")
-time.sleep(10)
+# f.close
+# a.close
+# print ("Wow u made it through all this shit!!")
+# time.sleep(10)
+
+print('SHeesh i was soo bad')
