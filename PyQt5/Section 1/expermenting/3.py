03d08df3fc16daefcf01cfddbc3e1cc3ed028b62
Advik <advik.b@gmail.com>
2021-12-10 13:09:25 +0000

    Update 3.py

--- PyQt5/Section 1/expermenting/3.py
+++ PyQt5/Section 1/expermenting/3.py
@@ -1,11 +1,30 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QCalendarWidget, QPushButton, QLineEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QCalendarWidget, QPushButton, QLineEdit, QProgressBar
 from PyQt5.QtGui import QFont
 from sys import argv, exit as sys_exit
+
+class CustomProgressBar(QProgressBar):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setStyleSheet("""
+            QProgressBar {
+                border: 2px solid grey;
+                border-radius: 5px;
+                text-align: center;
+            }
+            QProgressBar::chunk {
+                background-color: #00AA00;
+                width: 10px;
+                margin: 0.5px;
+            }
+        """)
+    
+    def step(self, val):
+        self.setValue(self.value() + val)
 
 class Window(QWidget):
     def __init__(self):
         super().__init__()
-        self.fon = QFont('Times', 20)
+        self.fon = QFont('Ubuntu Mono', 20)
         self.init_ui()
 
     def init_ui(self):
@@ -26,13 +45,25 @@ class Window(QWidget):
         self.text_edit.move(0, 300)
         self.text_edit.setText(self.calendar.selectedDate().toString())
         self.text_edit.setFont(self.fon)
+        self.text_edit.setReadOnly(True)
         self.text_edit.resize(800, 60)
+
+        self.download_button = QPushButton('Download', self)
+        self.download_button.move(0, 400)
+        self.download_button.resize(800, 60)
+        self.download_button.setFont(self.fon)
         
+        self.progress_bar = CustomProgressBar(self)
+        self.progress_bar.move(0, 500)
+        self.progress_bar.resize(800, 30)
+
         self.show()
 
     def show_date(self):
         print(self.calendar.selectedDate().toString())
+        self.text_edit.setDisabled(False)
         self.text_edit.setText(self.calendar.selectedDate().toString())
+        self.text_edit.setReadOnly(True)
     
 def main():
     app = QApplication(argv)
